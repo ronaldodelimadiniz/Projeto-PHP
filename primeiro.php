e5e30d8f69f8353f557aa78ff1fddc58cb2e40a4
ronaldodelimadiniz <ronaldo.diniz@etec.sp.gov.br>
2026-03-10 19:22:36 -0300

    projeto php

---
+++ primeiro.php
@@ -0,0 +1,5 @@
+<?php 
+	echo "Primeiro PHP";
+	//variavel de ambiente
+	$num = 10;
+ ?>
---
+++ terceiro.php
@@ -0,0 +1,10 @@
+<?php 
+
+	$num1 = 10;
+	$num2 = 20;
+
+	$resp = $num1 + $num2;
+
+	echo "A soma de num1: $num1 + num2: $num2 é: $resp";
+
+ ?>
